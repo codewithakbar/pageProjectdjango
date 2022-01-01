47530d9fa7656ac7eaade461c49cdffc51c1b64c
AkbarAli <88106359+DataAkbar@users.noreply.github.com>
2022-01-01 23:22:58 +0500

    Index, base, ozgardi

--- pageapp/urls.py
+++ pageapp/urls.py
@@ -4,7 +4,7 @@ from .views import AboutPageView, ContactPageView, HomePageView, MenuPageView, I
 urlpatterns = {
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
-    path('', HomePageView.as_view(), name='home'),
+    path('', IndexPageView.as_view(), name='home'),
     path('menu/', MenuPageView.as_view(), name='menu'),
     path('index/', IndexPageView.as_view(), name='index'),
 }
